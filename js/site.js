761815f8aa3c7293e9d0a3fcd7ba6c2ba506f4c9
Alvin Xiao <xiao.alvin@gmail.com>
2012-04-18 23:43:38 -0700

    Redid Demo Site

--- js/site.js
+++ js/site.js
@@ -1,10 +1,46 @@
-function init(){
-    $('#navigation').tabs();
-    $(".demo").button();
-}
+/* JS originally from fooljs.com */
 
-$(document).ready(function() {
-   init(); 
+$(function() {
+	var nav = $('nav'),
+		list = nav.find('li'),
+		
+		panels = $('.panels'),
+		
+		body = $('html, body');
+	
+	//  Set the first link as active
+	list.first().addClass('active');
+	
+	//  Handle other link clicks
+	list.children('a').click(function() {
+		var offset = $($(this).attr('href')).position();
+		
+		//  Move the divs
+		if(offset && offset.top) {
+			//  Change the active pointer
+			$(this).parent().addClass('active').siblings().removeClass('active');
+			
+			!body.is(':animated') && body.animate({scrollTop: offset.top - 60}, 500);
+		}
+
+		return false;
+	});
+
+	$(window).scroll(function() {
+		var me = $(this),
+			el = panels.children('div').eq(Math.round(me.scrollTop() / me.height()));
+				
+		list.find('a[href=#' + el.attr('id') + ']').parent().addClass('active').siblings().removeClass('active');
+	});
+	
+	$('pre a').click(function() {
+		var me = $(this),
+			action = me.text().split(':')[0];
+		
+		me.toggleClass('toggled');
+		
+		$.fool(action);
+		
+		return false;
+	});
 });
-
-
